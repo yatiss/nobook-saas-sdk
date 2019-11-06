0e156f89bb7df85834e907985b65d0b411e44f4b
jinxueqiang <haojinxueqiang@126.com>
2019-11-06 09:39:54 +0800

    add version v2.1.13

--- nobook/lab/index.js
+++ nobook/lab/index.js
@@ -1,6 +1,6 @@
 import { LabSDK } from './lab-sdk';
-import { APPKEY_TEST, APPSECRET_TEST, MESSAGE_TYPE, PID_TYPE } from '../config';
-export { APPKEY_TEST, APPSECRET_TEST, LabSDK, PID_TYPE, MESSAGE_TYPE };
+import { APPKEY_TEST, APPSECRET_TEST, MESSAGE_TYPE, PID_TYPE, PID_VALUE } from '../config';
+export { APPKEY_TEST, APPSECRET_TEST, LabSDK, PID_VALUE, PID_TYPE, MESSAGE_TYPE };
 if (typeof window === 'object') {
     window.NB_SDK_LAB = {
         APPKEY_TEST: APPKEY_TEST,
